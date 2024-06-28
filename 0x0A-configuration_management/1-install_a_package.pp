6db6972add19f44f5202fa3cab6de96304f4e19b
Bigsam08 <uelsam994@gmail.com>
2024-06-28 16:18:46 -0700

    werkeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install puppet-lint
-package { 'puppet-lint':
-  ensure          =>  'installed',
-  install_options =>  ['-v', '2.1.1'],
+# Using pip3 install flask version 2.1.0
+package { 'flask':
+  ensure          =>  '2.1.0',
+  provider =>  'pip3',
 }
