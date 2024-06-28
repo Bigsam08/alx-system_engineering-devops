ab7d9af6767960843f9da38fdb5988386d8028db
Bigsam08 <uelsam994@gmail.com>
2024-06-28 16:53:16 -0700

    fixing task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using pip3 install flask version 2.1.0
-package { 'flask':
-  ensure          =>  '2.1.0',
-  provider =>  'pip3',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
