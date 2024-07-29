ec6f2b77733e643bf258d5083790cf52632c746c
Bigsam08 <uelsam994@gmail.com>
2024-07-29 15:35:18 -0700

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+     export employee data tasks into CSV FORMAT
+     Comma Seperated Values
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    emp_id = sys.argv[1]
+    response = requests.get(url + "users/{}".format(emp_id))
+    data = response.json()
+
+    todo = requests.get(url + "todos?userId={}".format(emp_id))
+    todo_data = todo.json()
+
+    with open("{}.csv".format(emp_id), "w", newline='') as csvfile:
+        col = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.dict(csvfile, col=col)
+
+        writer.writeheader()
+        for task in todo_data:
+            writer.writerow({
+                "USER_ID": emp_id,
+                "USERNAME": data['name'],
+                "TASK_COMPLETED_STATUS": task['completed'],
+                "TASK_TITLE": task['title']
+                })
