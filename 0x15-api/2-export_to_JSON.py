45e85693cc54d4302fa03e44f74a5f157e27445c
Bigsam08 <uelsam994@gmail.com>
2024-07-29 16:54:48 -0700

    json format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Export employee data tasks into  JSON FORMAT
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(url + "users/{}".format(emp_id)).json()
+    username = user_data.get("username")
+    todo_data = requests.get(url + "todos", params={"userId": emp_id}).json()
+
+    with open("{}.json".format(emp_id), "w") as jsonfile:
+        for task in todo_data:
+            json.dump({emp_id: [{
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": username
+                }]}, jsonfile)
