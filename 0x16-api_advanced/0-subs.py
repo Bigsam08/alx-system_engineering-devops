b13a54b98c9dee8da45b81239bd924d96c07f45b
bigsam08 <uelsam994@gmail.com>
2024-08-13 22:27:22 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "MyUser)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "UserAgent"},
+                            allow_redirects=False)
     if response.status_code == 404:
         return 0
-    sub_list = response.json().get("data")
-    return sub_list.get("subscribers")
+
+    return response.json().get("data").get("subscribers")
